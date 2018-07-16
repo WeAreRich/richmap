e4c3fa34c8bfb153f4e0b144ab7de2be5350e474
文向东\wenxi <161250060@smail.nju.edu.cn>
2018-07-17 01:49:31 +0800

    vue init

--- client-vue/poverty-map/src/main.js
+++ client-vue/poverty-map/src/main.js
@@ -2,9 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
