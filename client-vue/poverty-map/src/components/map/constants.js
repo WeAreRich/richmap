5b3a101a37a97d1ccf89f481d77d5d290f3aef25
文向东\wenxi <161250060@smail.nju.edu.cn>
2018-07-17 00:51:37 +0800

    clean

--- client-vue/poverty-map/src/components/map/constants.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-const MAPBOX_TOKEN = 'pk.eyJ1Ijoid2VueGlhbmdkb25nIiwiYSI6ImNqajEyN2owYjBvNHYzdm10c2p2NXowM2kifQ.kixIr8Jwk2mAqmlQCWW88Q';
-export { MAPBOX_TOKEN };
