70102631e39b41630580f2e015b7adaf0f9ace13
161250071 <161250071@smail.nju.edu.cn>
2018-07-17 13:26:01 +0800

    ts

---
+++ client-vue/src/main.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import iview from 'iview'
+
+Vue.config.productionTip = false
+Vue.use(iview)
+export default new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(App)
+})
---
+++ client-vue/src/vue-shim.d.ts
@@ -0,0 +1,17 @@
+
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
+}
+// import * as lodash from 'lodash'
+// declare global {
+//   const _: typeof lodash
+// }
+
+// iview 全局方法
+declare module 'vue/types/vue' {
+  interface Vue {
+    $Message: any,
+    $Modal: any
+  }
+}
