edf1ee24a75be9407602eea041c1f5b2afb7a360
surevil <445073309@qq.com>
2018-07-18 15:27:46 +0800

    add layout structure

--- client-vue/src/router/index.ts
+++ client-vue/src/router/index.ts
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home.vue'
+import { BaseLayout } from '@/pages/BaseLayout.vue'
 
 Vue.use(Router);
 
 const routes = [
   {path: '/', component: Home}
 ];
+
+Vue.component('base-layout', BaseLayout);
 
 export default new Router({
   routes
