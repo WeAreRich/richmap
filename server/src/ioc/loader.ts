4450a7d215b49691457d7473f771030ef3484541
161250071 <161250071@smail.nju.edu.cn>
2018-07-20 12:56:02 +0800

    update test

--- server/src/ioc/loader.ts
+++ server/src/ioc/loader.ts
@@ -1,5 +1,6 @@
 // when a module is loaded @provide() will automatically register it
 import '../controller/home';
+import '../controller/DistrictNumber'
 import '../controller/user';
 import '../service/user';
 import '../service/DistrictService'
