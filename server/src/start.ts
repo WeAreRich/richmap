ec65b7f32b2a601e096da47ab7164649a6e4fd3c
161250071 <161250071@smail.nju.edu.cn>
2018-07-21 17:19:24 +0800

    服务器

--- server/src/start.ts
+++ server/src/start.ts
@@ -35,7 +35,7 @@ server.setConfig((app) => {
 
 let app = server.build();
 
-app.listen(3000);
+app.listen(80);
 
 console.log('Server started on port 80 :)');
 console.log("click http://127.0.0.1:3000");
