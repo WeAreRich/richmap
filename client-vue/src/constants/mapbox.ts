f7c655105474159fdc7b8f02368f1976c65e1811
文向东\wenxi <161250060@smail.nju.edu.cn>
2018-08-26 20:19:14 +0800

    新增四个tilesets放在mapbox.ts中

--- client-vue/src/constants/mapbox.ts
+++ client-vue/src/constants/mapbox.ts
@@ -7,6 +7,13 @@ let mapStyles = {
   'night-light-2001': 'mapbox://styles/wenxiangdong/cjjuyolqw21sf2rmmrrp13azv',
   'night-light-2013': 'mapbox://styles/wenxiangdong/cjjuzjzid0a1a2ro45v4abumf'
 };
+
+let tileSets = {
+  'DEM': 'mapbox://wenxiangdong.bp3u4q4m',
+  'GDP': 'mapbox://wenxiangdong.d3cpxjy6',
+  'slope': 'mapbox://wenxiangdong.3yngbsb6',
+  'nightLight': 'mapbox://wenxiangdong.527q7f2p'
+};
 const REC_LAYER_ID = 'rec-layer-id';
 const LINE_LAYER_ID = 'line-layer-id';
-export { ACCESS_TOKEN, CHINA_BOUNDS, CHINA_CENTER, mapStyles, REC_LAYER_ID, LINE_LAYER_ID,HUBEI_BOUNDS };
+export { ACCESS_TOKEN, CHINA_BOUNDS, CHINA_CENTER, mapStyles, REC_LAYER_ID, LINE_LAYER_ID, HUBEI_BOUNDS, tileSets };
