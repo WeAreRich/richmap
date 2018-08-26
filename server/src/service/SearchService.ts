25f71bcbabab5e7096791e9ce64d9540ee9739ad
tonywang1945yes <wangruihua88@yahoo.com>
2018-08-26 18:38:45 +0800

    现在能爬的所有网站
    Signed-off-by: tonywang1945yes <wangruihua88@yahoo.com>

--- server/src/service/SearchService.ts
+++ server/src/service/SearchService.ts
@@ -21,7 +21,13 @@ export class SearchService {
      let result:SearchResult[] = [];
      await this.getBaiduItem(kw,result);
      await this.getWeixinItem(kw,result);
-     // await this.getBaiduNewsItem(kw,result);
+     await this.getBaiduNewsItem(kw,result);
+     await this.getSogoItem(kw,result);
+     await this.getZhihuItem(kw,result);
+     await this.stats_gov_cn(kw,result);
+     await this.getYouChengItem(kw,result);
+     await this.getJianPinItem(kw,result);
+     await this.getLeadGroupItem(ke,result);
      return result;
   }
   
@@ -49,6 +55,134 @@ export class SearchService {
     resultArray.push(value)
   }
 
+    public async getSogoItem(kw: String, resultArray:SearchResult[]){
+        let options = {
+            method: 'get',
+            url: encodeURI("https://www.sogou.com/web?query="+kw)
+        };
+        let value: SearchResult = new SearchResult();
+        value.kind = '搜狗搜索';
+        let result:SearchItem[] = [];
+        let body = await this.rp(options);
+        console.log(body);
+        let $ = this.cheerio.load(body);
+        $('.sc_content').each(function(i, elem) {
+            let item:SearchItem = new SearchItem();
+
+            result.push(item);
+
+        });
+        value.result = result;
+        resultArray.push(value)
+    }
+
+    public async getZhihuItem(kw: String, resultArray:SearchResult[]){
+        let options = {
+            method: 'get',
+            url: encodeURI("https://www.zhihu.com/search?type=content&q="+kw)
+        };
+        let value: SearchResult = new SearchResult();
+        value.kind = '知乎';
+        let result:SearchItem[] = [];
+        let body = await this.rp(options);
+        console.log(body);
+        let $ = this.cheerio.load(body);
+        $('.sc_content').each(function(i, elem) {
+            let item:SearchItem = new SearchItem();
+
+            result.push(item);
+
+        });
+        value.result = result;
+        resultArray.push(value)
+    }
+
+    public async stats_gov_cn(kw: String, resultArray:SearchResult[]){
+        let options = {
+            method: 'get',
+            url: encodeURI("http://www.stats.gov.cn/was5/web/search?channelid=288041&andsen="+kw)
+        };
+        let value: SearchResult = new SearchResult();
+        value.kind = '国家统计局';
+        let result:SearchItem[] = [];
+        let body = await this.rp(options);
+        console.log(body);
+        let $ = this.cheerio.load(body);
+        $('.sc_content').each(function(i, elem) {
+            let item:SearchItem = new SearchItem();
+
+            result.push(item);
+
+        });
+        value.result = result;
+        resultArray.push(value)
+    }
+
+    public async getYouChengItem(kw: String, resultArray:SearchResult[]){
+        let options = {
+            method: 'get',
+            url: encodeURI("http://www.youcheng.org/search.php?search_key="+kw)
+        };
+        let value: SearchResult = new SearchResult();
+        value.kind = '友成基金会';
+        let result:SearchItem[] = [];
+        let body = await this.rp(options);
+        console.log(body);
+        let $ = this.cheerio.load(body);
+        $('.sc_content').each(function(i, elem) {
+            let item:SearchItem = new SearchItem();
+
+            result.push(item);
+
+        });
+        value.result = result;
+        resultArray.push(value)
+    }
+
+    public async getJianPinItem(kw: String, resultArray:SearchResult[]){
+        let options = {
+            method: 'get',
+            url: encodeURI("https://www.jianpincn.com/skwx_jp/Search.aspx?type=all&title=All&KeyWords="+kw)
+        };
+        let value: SearchResult = new SearchResult();
+        value.kind = '中国减贫研究数据库';
+        let result:SearchItem[] = [];
+        let body = await this.rp(options);
+        console.log(body);
+        let $ = this.cheerio.load(body);
+        $('.sc_content').each(function(i, elem) {
+            let item:SearchItem = new SearchItem();
+
+            result.push(item);
+
+        });
+        value.result = result;
+        resultArray.push(value)
+    }
+
+    public async getLeadGroupItem(kw: String, resultArray:SearchResult[]){
+        let options = {
+            method: 'get',
+            url: encodeURI("http://www.cpad.gov.cn/jsearch/search.do?pagemode=result&appid=1&webid=1&style=1&pos=title%2Ccontent&pg=10&ck=o&tmp_od=0&q="+kw)
+        };
+        let value: SearchResult = new SearchResult();
+        value.kind = '国务院扶贫领导小组办公室';
+        let result:SearchItem[] = [];
+        let body = await this.rp(options);
+        console.log(body);
+        let $ = this.cheerio.load(body);
+        $('.sc_content').each(function(i, elem) {
+            let item:SearchItem = new SearchItem();
+
+            result.push(item);
+
+        });
+        value.result = result;
+        resultArray.push(value)
+    }
+
+
+
   public async getBaiduNewsItem(kw:String,resultArray:SearchResult[]){
       let options = {
           method: 'get',
@@ -63,8 +197,7 @@ export class SearchService {
       let body = await this.rp(options);
       console.log(body);
       let $ = this.cheerio.load(body);
-      // console.log($('.result'));
-      // console.log("#####");
+
       this.request(options, function (err, res, body) {
           if (err) {
               console.log(err)
@@ -73,9 +206,9 @@ export class SearchService {
           }
       });
       $('.result').each(function(i, elem) {
-          console.log("11111");
+
           let item:SearchItem = new SearchItem();
-          item.href = "http://news.baidu.com"+($(this).children(".c-title").children('a').attr('href'));
+          item.href = ($(this).children(".c-title").children('a').attr('href'));
           item.title = ($(this).children(".c-title").children('a').text());
 
           item.abstract_info = ($(this).children(".c-summary c-row ").text());
