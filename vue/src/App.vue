74e7573c376c93bb1c94f8951dba33d0255e80d2
文向东\wenxi <161250060@smail.nju.edu.cn>
2018-08-30 15:31:10 +0800

    js vue

---
+++ vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Button>click</Button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import {Button} from 'iview'
+export default {
+  name: 'App',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
