9988366b8f6e70d0d86c9c6ad8f671a27dd17728
文向东\wenxi <161250060@smail.nju.edu.cn>
2018-08-30 21:24:56 +0800

    在地址跳转前加载  进度条，显示在见面上部

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,14 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'iview/dist/styles/iview.css'
-// import promise from 'es6-promise';
 import "@babel/polyfill";
 
 import {Message, Spin} from 'iview';
 
 Vue.config.productionTip = false;
 
-// promise.polyfill();
 
 Vue.prototype.$Message = Message;
 Vue.prototype.$Spin = Spin;
@@ -22,4 +20,4 @@ new Vue({
   router: router,
   components: { App },
   template: '<App/>'
-})
+});
