5afd8a8cdfb166ed2d32844dae3a4beddeda46ab
surevil <445073309@qq.com>
2018-10-28 21:42:31 +0800

    about

--- vue/src/components/common/About.vue
+++ vue/src/components/common/About.vue
@@ -1,168 +1,153 @@
 <template>
-    <div>
-        <div style="background-color: #fff;padding-top: 10%;padding-bottom: 10%">
-            <div style="background: url('https://img.alicdn.com/tps/TB1tA1KPpXXXXbhXXXXXXXXXXXX-208-236.png') no-repeat center center;background-size: 104px 118px;">
-                <div style="text-align:center;font-size: 32px;color: #373D41;font-weight: 700;line-height: 232px;">
-                    简介
-                </div>
+    <div style="margin-top: 80px">
+        <div style="line-height: 84px;font-size: 36px;font-weight: bold;text-align: center;color: #4849a8">关于我们</div>
+        <hr color="#575ea8"/>
+        <div style="text-align: center;font-size: 20px;line-height: 54px;font-weight: bold">
+            <p style="color: #575ea8;display: inline-block">多途径、高精度，助力</p>
+            <p style="color: orange;display: inline-block">精准扶贫和乡村振兴</p>
+        </div>
+        <hr color="#575ea8"/>
+        <div style="background-color: #fff;padding-bottom: 100px">
+            <div style="text-align: center;font-size: 20px;line-height: 54px;color: #575ea8;">
+                <p style="display:inline-block;font-weight:bold;font-size: 32px;line-height: 232px;">
+                    NO.1</p>
+                <p style="display:inline-block;font-size: 32px;line-height: 232px;">
+                    简介</p>
             </div>
             <div style="padding-left: 10%;padding-right: 10%;font-size: 14px;color: #4a4a4a;line-height: 24px;">
                 <h3>
                     VSR贫困地图系统将GIS技术应用到贫困识别、贫困致因分析中去，可以达成空间数据获取、管理、分析、模拟和可视化表达于一体的功能，将地理数据和多维贫困指标结合起来，使用ArcGIS等软件模拟出分辨率达到1km的空间贫困地图，目前已达到百米精度。目前“中国贫困在线”已正式上线，后期将通过增强现实（AR）的手段使贫困地图从线上走向线下，发挥更为直观和微观的作用，并增加AR移动端应用。</h3>
             </div>
         </div>
-        <div style="padding-top: 10%;padding-bottom: 10%;">
-            <div style="background: url('https://img.alicdn.com/tps/TB1bzWlPpXXXXaSXVXXXXXXXXXX-232-236.png') no-repeat center center;background-size: 104px 118px;">
-                <div style="text-align:center;font-size: 32px;color: #373D41;font-weight: 700;line-height: 232px;">
-                    我们的优势
-                </div>
+        <div class="about2">
+            <div style="text-align: center;font-size: 20px;line-height: 54px;color: #575ea8;">
+                <!--<img src="../../assets/images/about_background_2.jpg"/>-->
+                <p style="display:inline-block;font-weight:bold;font-size: 32px;line-height: 232px;">
+                    NO.2</p>
+                <p style="display:inline-block;font-size: 32px;line-height: 232px;">
+                    我们的优势</p>
             </div>
             <div style="padding-left: 12%;padding-right: 12%;">
-                <h3>
-                    <ul>
-                        <li>7个主题数据库</li>
-                        <li>多维贫困指标体系</li>
-                        <li>调研资料已覆盖中国10个省区</li>
-                        <li>专业的指导团队和执行团队</li>
-                        <li>相比传统的贫困识别、检测方法，我们使用客观数据，科学性更强，同时可以实现贫困的可视化，更易于操作</li>
-                        <li>具有显著的公益领域互联网地图特点</li>
-                    </ul>
+                <h3 style="padding-bottom: 50px">
+                    <Card class="about2-li">
+                        7个主题数据库
+                    </Card>
+                    <Card class="about2-li">
+                        多维贫困指标体系
+                    </Card>
+                    <Card class="about2-li">
+                        调研资料已覆盖中国10个省区
+                    </Card>
+                    <Card class="about2-li">
+                        专业的指导团队和执行团队
+                    </Card>
+                    <Card class="about2-li">
+                        相比传统的贫困识别、检测方法，我们使用客观数据，科学性更强，同时可以实现贫困的可视化，更易于操作
+                    </Card>
+                    <Card class="about2-li">
+                        具有显著的公益领域互联网地图特点
+                    </Card>
                 </h3>
             </div>
         </div>
-        <div style="background-color: #161a1d;padding-top: 10%;padding-bottom: 10%">
-            <div style="background: url('https://img.alicdn.com/tps/TB1vE1BPpXXXXa_XFXXXXXXXXXX-232-236.png') no-repeat center center;background-size: 104px 118px;">
-                <div style="text-align: center;font-size: 32px;color: #fff;font-weight: 700;line-height: 160px;">
-                    发展愿景
-                </div>
+        <div style="background-color: #fff">
+            <div style="text-align: center;font-size: 20px;line-height: 54px;color: #575ea8;">
+                <p style="display:inline-block;font-weight:bold;font-size: 32px;line-height: 232px;">
+                    NO.3</p>
+                <p style="display:inline-block;font-size: 32px;line-height: 232px;">
+                    发展愿景</p>
             </div>
-            <div style="padding-left: 10%;padding-right: 10%;font-size: 14px;color: grey;line-height: 24px;">
+            <div style="padding-left: 10%;padding-right: 10%;font-size: 14px;color: #4a4a4a;line-height: 24px;text-align: center">
                 <h3>
                     做地图产品中助贫服务最精准，助贫服务中地图产品最专业的，助力“精准扶贫”，打造第一助贫服务平台。</h3>
             </div>
         </div>
-        <div style="background: url('https://img.alicdn.com/tps/TB1gvqTPpXXXXa8XXXXXXXXXXXX-238-236.png') no-repeat center center;background-size: 104px 118px;">
-            <div style="text-align:center;font-size: 32px;color: #373D41;font-weight: 700;line-height: 232px;">
-                <h1>合作单位</h1>
+        <div style="background-color: #fff;padding-bottom: 50px;">
+            <div style="text-align: center;font-size: 20px;line-height: 54px;color: #575ea8;">
+                <p style="display:inline-block;font-weight:bold;font-size: 32px;line-height: 232px;">
+                    NO.4</p>
+                <p style="display:inline-block;font-size: 32px;line-height: 232px;">
+                    支持</p>
             </div>
-        </div>
-        <div style="text-align: center;margin-top: 3%;">
-            <h2>挂靠单位</h2>
-            <Row>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/MSW_logo.png"/>
+            <div style="padding-left: 10%;padding-right: 10%;font-size: 14px;color: #4a4a4a;line-height: 24px;">
+                <div style="text-align: center">
+                    <Card class="card-box1">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/MSW_logo.png"/>
+                            <div>
+                                <div class="float-box-text1">南京大学MSW中心</div>
+                            </div>
                         </div>
-                        <div>
-                            <div class="float-box-text">南京大学MSW中心</div>
+                    </Card>
+                    <Card class="card-box1">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/aide_logo.png"/>
+                            <div>
+                                <div class="float-box-text1">爱德基金会</div>
+                            </div>
                         </div>
-                    </div>
-                </Card>
-                </Col>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/aide_logo.png"/>
+                    </Card>
+                    <Card class="card-box1">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/shediao_logo.png"/>
+                            <div>
+                                <div class="float-box-text1">成都理工大学社调数研</div>
+                            </div>
                         </div>
-                        <div>
-                            <div class="float-box-text">爱德基金会</div>
+                    </Card>
+                    <Card class="card-box1">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/chengdupuzhong_logo.png"/>
+                            <div>
+                                <div class="float-box-text1" style="font-size: 12px;">成都普众市场调查有限公司</div>
+                            </div>
                         </div>
-                    </div>
-                </Card>
-                </Col>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/shediao_logo.png"/>
+                    </Card>
+                </div>
+                <div style="text-align: center;margin-top: 3%;">
+                    <Card class="card-box2">
+                        <div class="float-box">
+                            <img class="layout-logo-container"
+                                 src="../../assets/logos/nanjinguniversitysociety_logo.png"/>
+                            <div>
+                                <div class="float-box-text2" style="font-size: 14px;">南京大学社会学院</div>
+                            </div>
                         </div>
-                        <div>
-                            <div class="float-box-text">成都理工大学社调数研</div>
+                    </Card>
+                    <Card class="card-box2">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/doublecreate_logo.png"/>
+                            <div>
+                                <div class="float-box-text2">南京大学双创办</div>
+                            </div>
                         </div>
-                    </div>
-                </Card>
-                </Col>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/chengdupuzhong_logo.png"/>
+                    </Card>
+                    <Card class="card-box2">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/dihai_logo.png"/>
+                            <div>
+                                <div class="float-box-text2" style="font-size: 8px;">南京大学地理与海洋科学学院</div>
+                            </div>
                         </div>
-                        <div>
-                            <div class="float-box-text">成都普众市场调查有限公司</div>
+                    </Card>
+                    <Card class="card-box2">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/shiwangme_logo.png"/>
+                            <div>
+                                <div class="float-box-text2">视网么</div>
+                            </div>
                         </div>
-                    </div>
-                </Card>
-                </Col>
-            </Row>
-        </div>
-        <div style="text-align: center;margin-top: 3%;">
-            <h2>支持单位</h2>
-            <Row>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/nanjinguniversitysociety_logo.png"/>
+                    </Card>
+                    <Card class="card-box2">
+                        <div class="float-box">
+                            <img class="layout-logo-container" src="../../assets/logos/aliyun_logo.png"/>
+                            <div>
+                                <div class="float-box-text2">阿里云</div>
+                            </div>
                         </div>
-                        <div>
-                            <div class="float-box-text">南京大学社会学院</div>
-                        </div>
-                    </div>
-                </Card>
-                </Col>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/doublecreate_logo.png"/>
-                        </div>
-                        <div>
-                            <div class="float-box-text">南京大学双创办</div>
-                        </div>
-                    </div>
-                </Card>
-                </Col>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/dihai_logo.png"/>
-                        </div>
-                        <div>
-                            <div class="float-box-text">南京大学地理与海洋科学学院</div>
-                        </div>
-                    </div>
-                </Card>
-                </Col>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/shiwangme_logo.png"/>
-                        </div>
-                        <div>
-                            <div class="float-box-text">视网么</div>
-                        </div>
-                    </div>
-                </Card>
-                </Col>
-                <Col span="12">
-                <Card class="card-box">
-                    <div class="float-box">
-                        <div class="layout-logo-container">
-                            <img src="../../assets/logos/aliyun_logo.png"/>
-                        </div>
-                        <div>
-                            <div class="float-box-text">阿里云</div>
-                        </div>
-                    </div>
-                </Card>
-                </Col>
-            </Row>
+                    </Card>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -178,30 +163,52 @@
 </script>
 
 <style scoped>
-    .card-box {
+    .card-box1 {
         text-align: center;
         margin: 50px;
+        height: 200px;
+        width: 200px;
+        display: inline-block;
+    }
+
+    .card-box2 {
+        text-align: center;
+        margin: 30px;
+        height: 200px;
+        width: 200px;
+        display: inline-block;
     }
 
     .float-box {
-        display: inline-block;
-        margin: 5%;
-        padding: 60px 90px;
+        /*display: inline-block;*/
         text-align: center;
     }
 
     .layout-logo-container {
-        display: flex;
-        text-align: center;
-        justify-content: center;
-        height: 150px;
-        width: auto;
+        height: 100px;
+        width: 100px;
     }
 
-    .float-box-text {
+    .float-box-text1 {
         padding-top: 20px;
         width: auto;
         font-size: 16px;
         font-weight: bold;
+    }
+
+    .float-box-text2 {
+        padding-top: 20px;
+        width: auto;
+        font-size: 16px;
+        font-weight: bold;
+    }
+
+    .about2 {
+        background: url(../../assets/images/about_background_2.jpg) no-repeat;
+        background-size: 100% 100%;
+    }
+
+    .about2-li {
+        margin-bottom: 10px;
     }
 </style>
